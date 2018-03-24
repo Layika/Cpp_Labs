8c48e66e0433cbcc87cc3bfde559067b533dddd6
Layika <mwoleksy@gmail.com>
2018-03-24 14:47:29 +0100

    WIP

--- lab4/Watched.cpp
+++ lab4/Watched.cpp
@@ -1,1 +1,25 @@
 #include "Watched.h"
+
+Watched::Watched() :size(0), capacity(10) {
+  watchers = new Watcher*[capacity];
+}
+
+void Watched::addWatcher(Watcher* toAdd) {
+  if (size >= capacity) expandWatchers(watchers);
+
+  watchers[size] = toAdd;
+  size++;
+}
+
+void Watched::expandWatchers(Watcher** watchers) {
+  Watcher** newWatchers = new Watcher*[capacity*2];
+  for (unsigned int i = 0; i < size; ++i)
+    newWatchers[i] = watchers[i];
+  watchers = newWatchers;
+
+  capacity = capacity*2;
+}
+
+void Watched::gotNewData(int x) {
+
+}
--- lab4/Watched.h
+++ lab4/Watched.h
@@ -7,8 +7,14 @@ class Watcher;
 
 class Watched {
 public:
+
+  Watched();
   void gotNewData(int x);
   void addWatcher(Watcher* toAdd);
+  void expandWatchers(Watcher** watchers);
 
 private:
+  Watcher** watchers;
+  unsigned int size;
+  unsigned int capacity;
 };
